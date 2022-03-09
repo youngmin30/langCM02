db3dc8664ab977dcd02befdafb6ff5ca6b748c46
jjymj <https://github.com/youngmin30/webpj-2022-.git>
2022-03-10 01:09:49 +0900

    29 게시물 작성시 게시판 주입

--- src/main/java/com/jym/langCM02/controller/ArticleController.java
+++ src/main/java/com/jym/langCM02/controller/ArticleController.java
@@ -6,6 +6,7 @@ import com.jym.langCM02.domain.Member;
 import com.jym.langCM02.dto.article.ArticleDTO;
 import com.jym.langCM02.dto.article.ArticleModifyForm;
 import com.jym.langCM02.dto.article.ArticleSaveForm;
+import com.jym.langCM02.dto.board.BoardDTO;
 import com.jym.langCM02.service.ArticleService;
 import com.jym.langCM02.service.BoardService;
 import com.jym.langCM02.service.MemberService;
@@ -26,39 +27,48 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ArticleController {
 
+
     private final ArticleService articleService;
     private final MemberService memberService;
     private final BoardService boardService;
 
-    @GetMapping("/articles/write")
-    public String showArticleWrite(Model model) {
+
+    @GetMapping("/boards/{id}/articles/write")
+    public String showArticleWrite(@PathVariable(name = "id")Long id, Model model) {
+
+        BoardDTO boardDetail = boardService.getBoardDetail(id);
+
+        model.addAttribute("board", boardDetail);
         model.addAttribute("articleSaveForm", new ArticleSaveForm());
+
         return "usr/article/write";
+
     }
 
-    @PostMapping("/articles/write")
-    public String doWrite(@Validated ArticleSaveForm articleSaveForm, BindingResult bindingResult, Model model, Principal principal) {
+   @PostMapping("/boards/{id}/articles/write")
+    public String doWrite(@Validated ArticleSaveForm articleSaveForm, BindingResult bindingResult, Model model, Principal principal, @PathVariable(name = "id")Long id) {
+
         if (bindingResult.hasErrors()) {
             return "usr/article/write";
         }
-
         try {
-
             Board findBoard = boardService.getBoard(articleSaveForm.getBoard_id());
             Member findMember = memberService.findByLoginId(principal.getName());
-
             articleService.save(
                     articleSaveForm,
                     findMember,
                     findBoard
             );
-
         } catch (IllegalStateException e) {
             model.addAttribute("err_msg", e.getMessage());
             return "usr/article/write";
+
         }
-        return "redirect:/";
+
+        return "redirect:/articles";
+
     }
+
     @GetMapping("/articles/modify/{id}")
     public String showModify(@PathVariable(name = "id") Long id, Model model){
         try {
@@ -82,13 +92,13 @@ public class ArticleController {
         }
     }
 
-    @GetMapping("/articles/")
+    @GetMapping("/articles")
     public String showList(Model model) {
+
         List<ArticleDTO> articleList = articleService.getList();
         model.addAttribute("articleList", articleList);
         return "usr/article/list";
     }
-
     @GetMapping("/articles/delete/{id}")
     public String deleteArticle(@PathVariable(name = "id") Long id, Principal principal){
         try {
@@ -102,7 +112,6 @@ public class ArticleController {
             return "redirect:/";
         }
     }
-
     @GetMapping("/articles/{id}")
     public String showDetail(@PathVariable(name = "id") Long id, Model model){
         try {
